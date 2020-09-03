107ac903d505853bc9e87be1ece05ebeccef180f
Miles Morales <pgsilva0698@gmail.com>
2020-09-02 22:01:50 -0300

    initial

---
+++ back/src/main/java/api/casamento/ApiCasamentoApplication.java
@@ -0,0 +1,13 @@
+package api.casamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCasamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCasamentoApplication.class, args);
+	}
+
+}
